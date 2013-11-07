df673b850bfa347f55279996b563f446d55f9533
jeff <icystraw1@hotmail.com>
2013-11-07 19:49:16 +1100

    finished order and orderhistory entity classes

--- StockTrack/Order.cs
+++ StockTrack/Order.cs
@@ -8,5 +8,82 @@ namespace StockTrack
 {
     class Order
     {
+        private int orderId;
+
+        public int OrderId
+        {
+            get { return orderId; }
+            set { orderId = value; }
+        }
+        private string orderNo;
+
+        public string OrderNo
+        {
+            get { return orderNo; }
+            set { orderNo = value; }
+        }
+        private string customerName;
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; }
+        }
+        private string contactNo;
+
+        public string ContactNo
+        {
+            get { return contactNo; }
+            set { contactNo = value; }
+        }
+        private string shipping;
+
+        public string Shipping
+        {
+            get { return shipping; }
+            set { shipping = value; }
+        }
+        private double totalAmount;
+
+        public double TotalAmount
+        {
+            get { return totalAmount; }
+            set { totalAmount = value; }
+        }
+        private double paidToday;
+
+        public double PaidToday
+        {
+            get { return paidToday; }
+            set { paidToday = value; }
+        }
+        private bool isWorkOrder;
+
+        public bool IsWorkOrder
+        {
+            get { return isWorkOrder; }
+            set { isWorkOrder = value; }
+        }
+        private DateTime orderDate;
+
+        public DateTime OrderDate
+        {
+            get { return orderDate; }
+            set { orderDate = value; }
+        }
+        private DateTime shippingDate;
+
+        public DateTime ShippingDate
+        {
+            get { return shippingDate; }
+            set { shippingDate = value; }
+        }
+        private string comments;
+
+        public string Comments
+        {
+            get { return comments; }
+            set { comments = value; }
+        }
     }
 }
--- StockTrack/OrderHistory.cs
+++ StockTrack/OrderHistory.cs
@@ -8,5 +8,33 @@ namespace StockTrack
 {
     class OrderHistory
     {
+        private int orderHistoryId;
+
+        public int OrderHistoryId
+        {
+            get { return orderHistoryId; }
+            set { orderHistoryId = value; }
+        }
+        private int orderId;
+
+        public int OrderId
+        {
+            get { return orderId; }
+            set { orderId = value; }
+        }
+        private DateTime historyDate;
+
+        public DateTime HistoryDate
+        {
+            get { return historyDate; }
+            set { historyDate = value; }
+        }
+        private string comments;
+
+        public string Comments
+        {
+            get { return comments; }
+            set { comments = value; }
+        }
     }
 }
